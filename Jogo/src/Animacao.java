30929f98bbd23543a13b250bd3cf1542ee5b2b2d
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-01-24 11:06:03 -0300

    Adição de movimento vertical e horizontal

--- Jogo/src/Animacao.java
+++ Jogo/src/Animacao.java
@@ -4,16 +4,32 @@ import java.awt.event.ActionListener;
 
 public class Animacao implements ActionListener{
 
-    private TelaObjeto tela;
+    private TelaObjeto bolinha;
 
-    public Animacao(TelaObjeto bolinha){
-        this.tela = bolinha;
+    public Animacao(TelaObjeto bolinha, Mouse mouse){
+        this.bolinha = bolinha;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-        tela.mover();
+        if (!bolinha.confereLargura()) {
+            bolinha.velocidadeHorizontal = bolinha.velocidadeHorizontal * -1;
+        }
+
+        if (!bolinha.confereAltura()) {
+            bolinha.velocidadeVertical = bolinha.velocidadeVertical * -1;
+        }
+
+        bolinha.xPos =+ bolinha.xPos+bolinha.velocidadeHorizontal;
+        bolinha.yPos =+ bolinha.yPos+bolinha.velocidadeVertical;
+
+        
+        
+        System.out.println("X Pos: "+ bolinha.getXpos()+ " Y Pos: "+bolinha.getYpos());
+        System.out.println("X Pos Final: "+ (bolinha.getXpos()+bolinha.getTamanho())+ " Y POs Final: "+(bolinha.getYpos()+bolinha.getTamanho()));
+        bolinha.repaint();
+        
     }
     
 }
