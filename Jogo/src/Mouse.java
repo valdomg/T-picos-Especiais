7bf3617bcc8615ba014091195040e743747afdb4
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-01-24 15:49:30 -0300

    correção de bugs e refatoração

--- Jogo/src/Mouse.java
+++ Jogo/src/Mouse.java
@@ -1,6 +1,9 @@
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
+
 
 
 
@@ -8,7 +11,9 @@ public class Mouse implements MouseListener{
 
     private TelaObjeto bolinha;
     private TelaJogo tela;
-    private Animacao animacao; 
+
+    private double mouseX;
+    private double mouseY;
 
     public Mouse(TelaObjeto bolinha, TelaJogo tela){
         this.bolinha = bolinha;
@@ -18,16 +23,30 @@ public class Mouse implements MouseListener{
     @Override
     public void mouseClicked(MouseEvent e) {
         // TODO Auto-generated method stub
-        Point xy = e.getPoint();
+        this.mouseX = e.getPoint().getX();
+        this.mouseY = e.getPoint().getY();
 
-        if (xy.getX() <= (bolinha.getXpos()+bolinha.getTamanho()) && xy.getX() >= bolinha.getXpos()) {
+        int bolinhaTamanhoX = bolinha.getTamanhoX();
+        int bolinhaX = bolinha.getXpos();
+
+        int bolinhaTamanhoY = bolinha.getTamanhoY();
+        int bolinhaY = bolinha.getYpos();
+
+
+
+        if ((mouseX >= bolinhaX && mouseX <= bolinhaTamanhoX) && (mouseY >= bolinhaY)) {
             System.out.println("Acertou miserable");
-            tela.atualizarPlacar();
+            bolinha.spawnar();
+            bolinha.aumentarVelocidade();
+            bolinha.atualizarPlacar();
         }
 
-        System.out.println("XMouse: "+ xy.getX()+ " YMouse: "+xy.getY());
-        System.out.println("X Pos: "+ bolinha.getXpos()+ " Y Pos: "+bolinha.getYpos());
-        //System.out.println("X: "+ (bolinha.getXpos()+bolinha.getTamanho())+ " Y: "+(bolinha.getYpos()+bolinha.getTamanho()));
+       
+        System.out.println("XMouse: "+ mouseX + " YMouse: "+mouseY);
+        System.out.println("XBola:" + bolinhaTamanhoX + " YBola: " + bolinhaTamanhoY);
+        //System.out.println("X Pos: "+ bolinha.getXpos()+ " Y Pos: "+bolinha.getYpos());
+        //System.out.println("X: "+ bolinha.getXpos()+ " X: "+(bolinha.getXpos()+bolinha.getTamanhoX()));
+        //System.out.println("Y: "+ bolinha.getYpos()+ " Y: "+(bolinha.getYpos()+bolinha.getTamanhoY()));
 
     }
 
@@ -47,7 +66,6 @@ public class Mouse implements MouseListener{
     @Override
     public void mouseEntered(MouseEvent e) {
         // TODO Auto-generated method stub
-        //System.out.println("Mouse entered");
     }
 
     @Override
