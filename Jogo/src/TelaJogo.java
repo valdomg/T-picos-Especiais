aefaaf59e3f177500dcce1a1f131fcac14244e6b
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-01-24 15:50:25 -0300

    refatoração

--- Jogo/src/TelaJogo.java
+++ Jogo/src/TelaJogo.java
@@ -1,7 +1,9 @@
+import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.Timer;
 
 public class TelaJogo extends JFrame{
@@ -9,31 +11,30 @@ public class TelaJogo extends JFrame{
     private TelaObjeto bolinha;
     private Timer timer;
     private Mouse mouse;
+    private TelaSprite sprites;
 
-    private String placar;
-
-    public TelaJogo(){
+    public TelaJogo(String nome){
+        super.setTitle(nome);
         setAlwaysOnTop(true);
         setAutoRequestFocus(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setResizable(false);
         setLocationRelativeTo(null);
-        setPreferredSize(new Dimension(600,600));
-
         
+
+        this.sprites = new TelaSprite();
         this.bolinha = new TelaObjeto();
         this.mouse = new Mouse(bolinha, this);
         this.timer = new Timer(10, new Animacao(bolinha, mouse));
 
-        timer.start();
         getContentPane().add(bolinha);
+        
         addMouseListener(mouse);
-
+        
+        
+        timer.start();
         pack();
     }
 
-    public void atualizarPlacar(){
-        
-    }
 }
